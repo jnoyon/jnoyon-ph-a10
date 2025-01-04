1801276981e30db22b60fe6875e56fea0ea09bca
jnoyon <jihadur51@gmail.com>
2025-01-04 15:07:17 +0600

    Slider Updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,35 +1,31 @@
 import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
 
-  const divStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundSize: 'cover',
-    height: '400px'
-  }
+
 const slideImages = [
     {
-      url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      url: 'https://tp.jihadur.com/wp-content/uploads/2025/01/banner.png',
     },
     {
-      url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+      url: 'https://tp.jihadur.com/wp-content/uploads/2025/01/banner-2.png',
     },
     {
-      url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      url: 'https://tp.jihadur.com/wp-content/uploads/2025/01/banner-3.png',
     },
   ];
 export default function Slider() {
   return (
-    <div className="slide-container">
+    <div className="container mx-auto w-11/12 mt-5">
+      <div className="slide-container">
         <Slide>
          {slideImages.map((slideImage, index)=> (
             <div key={index}>
-              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+              <div className='w-full rounded-md h-48 md:h-[500px] bg-cover' style={{ 'backgroundImage': `url(${slideImage.url})` }}>
               </div>
             </div>
           ))} 
         </Slide>
       </div>
+    </div>
   )
 }
